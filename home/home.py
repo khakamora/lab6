936afcf89f208ffe47992ec4a9a10826ea67cd80
khakanora <yuriy.mamchurovskyy@gmail.com>
2018-11-15 13:38:50 +0200

    csv reader

--- home/home.py
+++ home/home.py
@@ -8,6 +8,7 @@ import csv
 from tkinter import ttk
 from distutils import command
 from tkinter import messagebox
+from tkinter import *
 
 
 root = tkinter.Tk()
@@ -22,13 +23,22 @@ with open('students.csv', newline = '', encoding='utf-8') as file:
       c = 0
       for row in col:
          
-         label = tkinter.Label(root, width = 30, height = 2, \
+         example = label = tkinter.Label(root, width = 30, height = 2, \
                                text = row, relief = tkinter.RIDGE)
          label.grid(row = r, column = c)
          c += 1
       r += 1
 
 content = ttk.Frame(root)
+
+def prep(event):
+    event.widget.config(bg='light blue')
+    event.widget.focus_set()  # give keyboard focus to the label
+    event.widget.bind('<Key>', edit)
+
+def edit(event):
+    print(event.char)
+
 
 def showDialog():
     #messagebox.showinfo("Say Hello", "Hello World")
@@ -50,11 +60,12 @@ def showDialog():
     e3.grid(row=2, column=1)
     e4.grid(row=3, column=1)
     
+    #tkinter.Button(window, text='Click', command=showSelected).grid(row=4, column=0, pady=4)
     tkinter.Button(window, text='Close', command=window.quit).grid(row=4, column=0, pady=4)
     tkinter.Button(window, text='Save').grid(row=4, column=1, pady=4)
 
 def showSelected():
-    messagebox.showinfo("Say Hello", "Hello World")
+    messagebox.showinfo("Say Hello", example.cget("text"))#
 
 show_info =  ttk.Button(content, text="Show", command=showSelected)
 edit = ttk.Button(content, text="Edit", command=showDialog)
@@ -67,4 +78,7 @@ quit.grid(column=c+2, row=r+1)
 
 
 root.geometry("1200x600")   
+example.grid()
+example.bind('<Button-1>', prep)
+
 root.mainloop()
--- test.py
+++ test.py
@@ -3,15 +3,33 @@ Created on 14 лист. 2018 р.
 
 @author: default-user
 '''
+#from tkinter import *
+#root = Tk()
+ 
+#def changeFont(font):
+ #   l['font'] = font
+ 
+#l = Label(text="Hello World")
+#l.pack()
+#Button(command=lambda f="Verdana": changeFont(f)).pack()
+#Button(command=lambda f="Times": changeFont(f)).pack()
+ 
+#root.mainloop()
+
 from tkinter import *
 root = Tk()
- 
-def changeFont(font):
-    l['font'] = font
- 
-l = Label(text="Hello World")
-l.pack()
-Button(command=lambda f="Verdana": changeFont(f)).pack()
-Button(command=lambda f="Times": changeFont(f)).pack()
- 
-root.mainloop()
+frame = Frame(root)
+frame.pack()
+
+def prep(event):
+    event.widget.config(bg='light blue')
+    event.widget.focus_set()  # give keyboard focus to the label
+    event.widget.bind('<Key>', edit)
+
+def edit(event):
+    print(event.char)
+
+example = Label(frame, text='Click me')
+example.pack()
+example.bind('<Button-1>', prep)
+mainloop()
